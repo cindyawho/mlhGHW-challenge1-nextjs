bc2d9835814301a4fb710b630a79fc1ec7fd59bb
Cindy Andrade <161978639+cindyawho@users.noreply.github.com>
2024-11-08 10:18:56 -0800

    Update page.tsx

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen min-w-screen flex-col items-center justify-center bg-gradient-defang">
       <h1 className="text-4xl font-bold text-white">Next.js &times; Defang</h1>
+      <p>Edited by CindyAWho on github.</p>
     </main>
   );
 }
